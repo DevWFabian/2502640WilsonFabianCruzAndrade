a195b841d92377440e43ed12b5f27ae61f8303ec
DevWFabian <wilsonfabian.cruzandrade@gmail.com>
2022-08-29 21:17:48 -0500

    se actualizo y se ajusto las tablas

--- payrollAppGUI/index.py
+++ payrollAppGUI/index.py
@@ -1,19 +1,18 @@
+from calendar import c
 import tkinter as tk
-from tkinter import ttk, messagebox
+from tkinter import CENTER, N, ttk, messagebox
 
 class Payroll(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title('Nomina')
-        self.geometry('750x400')
-        self.resizable(0,0)
         self.create_tabs()
     
     def createEmployee(self,tabulador):
         frame= tk.LabelFrame(tabulador,text='Ingresar nuevo empleado',font=15)
-        frame.grid(row = 0 ,column = 1, columnspan = 3,pady = 20, padx=40)
+        frame.grid(row = 0 ,column = 1, columnspan = 3,pady = 20, padx=100)
         # nombre
-        tk.Label(frame , text = 'Nombres: ',font=15,padx=40).grid(row=1,column=0,padx=20,pady=10)
+        tk.Label(frame , text = 'Nombres: ',font=15,padx=40,justify="left").grid(row=1,column=0,padx=20,pady=10)
         self.firstName= tk.Entry(frame, width=40,font=15)
         self.firstName.grid(row = 1 , column = 1, padx=20)
         # Apellidos
@@ -33,19 +32,20 @@ class Payroll(tk.Tk):
         self.documentNumber= tk.Entry(frame, width=40,font=15)
         self.documentNumber.grid(row = 4 , column = 1, padx=20)
         # Salario base
-        tk.Label(frame, text='Salario Base',font=15, padx=40).grid(row=5,column=0,padx=20,pady=10)
+        tk.Label(frame, text='Salario Base:',font=15, padx=40).grid(row=5,column=0,padx=20,pady=10)
         self.baseSalary= tk.Entry(frame, width=40,font=15)
         self.baseSalary.grid(row = 5 , column = 1, padx=20)
         boton1 = ttk.Button(tabulador, text='Registrar')
         boton1.grid(row=1, column=2)
     def readEmployees(self,tabulador):
-        self.geometry('900x400')
-        self.tree=ttk.Treeview(tabulador,height=60,columns=2)
-        self.tree.grid(row=0,column=0,columnspan=1)
-        self.tree=ttk.Treeview(tabulador,height=60,columns=2)
-        self.tree.grid(row=0,column=2,columnspan=1)
-        self.tree=ttk.Treeview(tabulador,height=60,columns=1)
-        self.tree.grid(row=0,column=4,columnspan=1)
+        self.tree=ttk.Treeview(tabulador,height=15,columns=("Nombres","Apellidos","Tipo de documento","Numero de documento","Salario base"))
+        self.tree.heading("#0",text="ID_Empleado");self.tree.column("#0",width=100,anchor=CENTER)
+        self.tree.heading("Nombres",text="Nombres");self.tree.column("Nombres",width=150,anchor=CENTER)
+        self.tree.heading("Apellidos",text="Apellidos");self.tree.column("Apellidos",width=150,anchor=CENTER)
+        self.tree.heading("Tipo de documento",text="Tipo de documento");self.tree.column("Tipo de documento",width=150,anchor=CENTER)
+        self.tree.heading("Numero de documento",text="Numero de documento");self.tree.column("Numero de documento",width=150,anchor=CENTER)
+        self.tree.heading("Salario base",text="Salario base");self.tree.column("Salario base",width=150,anchor=CENTER)
+        self.tree.grid(row=0,column=0,columnspan=1,padx=20,pady=20)
         
         
     def create_tabs(self):
